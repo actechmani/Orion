9dafc135fdc22fdf90c95bef724ecc6717310cb7
infa-sbalasubramanian <sbalasubramanian@informatica.com>
2023-09-07 20:07:44 +0530

    done

--- src/app/utils/http/index.ts
+++ src/app/utils/http/index.ts
@@ -1,70 +1,70 @@
-// import HttpInterceptor from './httpInterceptor'
+import HttpInterceptor from './httpInterceptor'
 
-// const baseURL = `${process.env.API_URL}:${process.env.API_PORT}/${process.env.API_BASE_URL}/`
+const baseURL = `${process.env.API_URL}:${process.env.API_PORT}/${process.env.API_BASE_URL}/`
 
-// export default class HTTPRequestHandler {
-//   static request = new HttpInterceptor(baseURL).getRequest()
+export default class HTTPRequestHandler {
+  static request = new HttpInterceptor(baseURL).getRequest()
 
-//   static get = (
-//     url: string,
-//     payload?: any, // Define the payload type or use 'any' if it can vary
-//     headers: Record<string, string> = {}
-//   ): Promise<any> =>
-//     HTTPRequestHandler.request.get(url, {
-//       data: payload, // Pass the payload as 'data' property
-//       headers: headers, // Pass the headers as 'headers' property
-//     })
+  static get = (
+    url: string,
+    payload?: any, // Define the payload type or use 'any' if it can vary
+    headers: Record<string, string> = {}
+  ): Promise<any> =>
+    HTTPRequestHandler.request.get(url, {
+      data: payload, // Pass the payload as 'data' property
+      headers: headers, // Pass the headers as 'headers' property
+    })
 
-//   static post = (
-//     url: string,
-//     payload?: any, // Define the payload type or use 'any' if it can vary
-//     headers: Record<string, string> = {}
-//   ): Promise<any> =>
-//     HTTPRequestHandler.request.post(url, {
-//       data: payload, // Pass the payload as 'data' property
-//       headers: headers, // Pass the headers as 'headers' property
-//     })
+  static post = (
+    url: string,
+    payload?: any, // Define the payload type or use 'any' if it can vary
+    headers: Record<string, string> = {}
+  ): Promise<any> =>
+    HTTPRequestHandler.request.post(url, {
+      data: payload, // Pass the payload as 'data' property
+      headers: headers, // Pass the headers as 'headers' property
+    })
 
-//   static put = (
-//     url: string,
-//     payload?: any, // Define the payload type or use 'any' if it can vary
-//     headers: Record<string, string> = {}
-//   ): Promise<any> =>
-//     HTTPRequestHandler.request.put(url, {
-//       data: payload, // Pass the payload as 'data' property
-//       headers: headers, // Pass the headers as 'headers' property
-//     })
-//   static delete = (
-//     url: string,
-//     payload?: any, // Define the payload type or use 'any' if it can vary
-//     headers: Record<string, string> = {}
-//   ): Promise<any> =>
-//     HTTPRequestHandler.request.delete(url, {
-//       data: payload, // Pass the payload as 'data' property
-//       headers: headers, // Pass the headers as 'headers' property
-//     })
+  static put = (
+    url: string,
+    payload?: any, // Define the payload type or use 'any' if it can vary
+    headers: Record<string, string> = {}
+  ): Promise<any> =>
+    HTTPRequestHandler.request.put(url, {
+      data: payload, // Pass the payload as 'data' property
+      headers: headers, // Pass the headers as 'headers' property
+    })
+  static delete = (
+    url: string,
+    payload?: any, // Define the payload type or use 'any' if it can vary
+    headers: Record<string, string> = {}
+  ): Promise<any> =>
+    HTTPRequestHandler.request.delete(url, {
+      data: payload, // Pass the payload as 'data' property
+      headers: headers, // Pass the headers as 'headers' property
+    })
 
-//   static patch = (
-//     url: string,
-//     payload?: any, // Define the payload type or use 'any' if it can vary
-//     headers: Record<string, string> = {}
-//   ): Promise<any> =>
-//     HTTPRequestHandler.request.patch(url, {
-//       data: payload, // Pass the payload as 'data' property
-//       headers: headers, // Pass the headers as 'headers' property
-//     })
+  static patch = (
+    url: string,
+    payload?: any, // Define the payload type or use 'any' if it can vary
+    headers: Record<string, string> = {}
+  ): Promise<any> =>
+    HTTPRequestHandler.request.patch(url, {
+      data: payload, // Pass the payload as 'data' property
+      headers: headers, // Pass the headers as 'headers' property
+    })
 
-//   static head = (
-//     url: string,
-//     payload?: any, // Define the payload type or use 'any' if it can vary
-//     headers: Record<string, string> = {}
-//   ): Promise<any> =>
-//     HTTPRequestHandler.request.head(url, {
-//       data: payload, // Pass the payload as 'data' property
-//       headers: headers, // Pass the headers as 'headers' property
-//     })
+  static head = (
+    url: string,
+    payload?: any, // Define the payload type or use 'any' if it can vary
+    headers: Record<string, string> = {}
+  ): Promise<any> =>
+    HTTPRequestHandler.request.head(url, {
+      data: payload, // Pass the payload as 'data' property
+      headers: headers, // Pass the headers as 'headers' property
+    })
 
-//   static redirectTo = (path) => {
-//     document.location = path
-//   }
-// }
+  static redirectTo = (path) => {
+    document.location = path
+  }
+}
